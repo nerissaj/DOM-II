8c1753da261d0e45c9c17f657b47c637b28db4bc
Nerissa Taylor <taylornerissa@hotmail.com>
2019-06-19 23:59:54 -0400

    fix stop propagation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 // Your code goes here
 //resize & load
 let resizePane = window.addEventListener("resize", function(){
-    console.log('resize!')}, true);
+    console.log('resize')}, true);
     console.log(`page fully loaded!`);
     //wheel
     var wheelEvent = new WheelEvent(null);
@@ -14,7 +14,7 @@ let resizePane = window.addEventListener("resize", function(){
 let blueLink = document.querySelector(".nav-link");
 blueLink.addEventListener('mouseover', function(event) {   
   // highlight the mouseover target
-  event.target.style.color = "orange";
+  event.target.style.color = "pink";
 console.log(`I was mouseover, this was my event:${event}`)
 });
 
@@ -26,34 +26,40 @@ console.log(`I was mouseover, this was my event:${event}`)
 
 //focus
 
-
-
 //scroll
 //select
-//let imgSelect =document.getElementsByClassName(".img-content");
-//imgSelect.addEventListener('select', function(event){
-  //  event.target.style.backgroundColor ="teal";
+//let imgSelect = document.querySelector(".img-content");
+//imgSelect.addEventListener('keydown', function(event){
+ //  event.target.style.backgroundColor ="teal";
 //console.log(`the background color is teal.`);
 //});
 //Stop Propagation
 const mainNavigation = document.querySelector(".main-navigation");
 mainNavigation.addEventListener('click', function(eventObject) {
-    console.log(`main nav fired!`);
-    eventObject.stopPropagation();
-    
- });
- //Prevent Default
- const nav = document.querySelector(".nav");
- nav.addEventListener('click', function(event) {
+  eventObject.stopPropagation();
+  eventObject.target.style.color = "green";
+  console.log(`main nav fired!`);
+  
+  
+});
+//Prevent Default
+const nav = document.querySelector(".nav");
+nav.addEventListener('click', function(eventObject) {
+  eventObject.stopPropagation();
+  eventObject.target.style.backgroundColor = "lightgrey";
+  eventObject.target.style.color = "yellow"
     console.log(`nav fired`);
-    event.preventDefault();
+    
+    eventObject.preventDefault();
+    
  });
 //double Click
 let DblBtn = document.querySelector(".btn");
 DblBtn.addEventListener('dblclick', function(event){
+  event.target.style.backgroundColor = "red";
     console.log(`I was doubleClicked this was my event:${event}`)
 });
-const imgContent = document.querySelector('.img-content');
-imgContent.addEventListener('keydown', function(event){
-console.log(`a key was pressed this was my event:${event}`);
-});
+//const  = document.getElementsByClassName('');
+//.addEventListener('keydown', function(event){
+//console.log(`a key was pressed this was my event:${event}`);
+//});
